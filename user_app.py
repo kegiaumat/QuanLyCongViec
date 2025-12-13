4f4aa3934ae10fd6cd8ceccd4147496139fb220f
kegiaumat <mrmmoonline@gmail.com>
2025-12-14 01:00:35 +0700

    Add files via upload

--- user_app.py
+++ user_app.py
@@ -180,7 +180,6 @@ def user_app(user):
                 gb.configure_column("Khối lượng (giờ)", width=120)
                 gb.configure_column("Ghi chú", flex=5)
                 gb.configure_column("Chọn?", width=80)
-                gb.configure_column("Chọn?", hide=True)
 
                 gb.configure_default_column(resizable=True, sortable=True, filter=True)
 
